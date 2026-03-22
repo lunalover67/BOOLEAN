a392051961ed29f2c2c6d6608c63a33408f0cf2d
worghet <woorghet@gmail.com>
2026-03-22 17:48:27 -0400

    made basic ui

--- app/src/main/java/com/example/aboolean/MainActivity.java
+++ app/src/main/java/com/example/aboolean/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.aboolean;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        clearSystemUI();
+
         setContentView(R.layout.activity_main);
     }
+
+    private void clearSystemUI() {
+
+        // [CLARITY] Gets rid of the top status bar.
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        // [CLARITY] Gets rid of the bottom navigation bar.
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+
+    }
+
+
+
 }
