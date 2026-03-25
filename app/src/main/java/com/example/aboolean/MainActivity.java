fae6cd03561297ad4e2588c04b8652247862ae8a
worghet <woorghet@gmail.com>
2026-03-25 19:25:19 -0400

    implemented basic logic; no connections yet

--- app/src/main/java/com/example/aboolean/MainActivity.java
+++ app/src/main/java/com/example/aboolean/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        // disable btns (both)
+        // disable btns (both) hiya
 
         // run flip animation (1-3s) :
 
